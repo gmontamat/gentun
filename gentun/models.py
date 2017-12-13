10884fa3219d9f2e92ef02fa34d2622805034522
Gustavo Montamat <gmontamat@gmail.com>
2017-12-13 11:35:33 -0300

    Fix parameters

--- gentun/models.py
+++ gentun/models.py
@@ -9,10 +9,9 @@ import xgboost as xgb
 
 class GentunModel(object):
 
-    def __init__(self, x_train, y_train, parameters):
+    def __init__(self, x_train, y_train):
         self.x_train = x_train
         self.y_train = y_train
-        self.params = parameters
 
     def cross_validate(self):
         raise NotImplementedError("Use a subclass with a defined model.")
@@ -20,16 +19,20 @@ class GentunModel(object):
 
 class XgboostRegressor(GentunModel):
 
-    def __init__(self, x_train, y_train, hyperparameters, eval_metric='rmse', nfold=5):
+    def __init__(self, x_train, y_train, hyperparameters, eval_metric='rmse', nfold=5,
+                 num_boost_round=5000, early_stopping_rounds=100):
+        super(XgboostRegressor, self).__init__(x_train, y_train)
         hyperparameters.update({
             'booster': 'gbtree',
             'objective': 'reg:linear',
             'eval_metric': eval_metric,
             'silent': 1
         })
-        super(XgboostRegressor, self).__init__(x_train, y_train, hyperparameters)
+        self.params = hyperparameters
         self.eval_metric = eval_metric
         self.nfold = nfold
+        self.num_boost_round = num_boost_round
+        self.early_stopping_rounds = early_stopping_rounds
 
     def cross_validate(self):
         """Train model using n-fold cross validation and
@@ -37,19 +40,20 @@ class XgboostRegressor(GentunModel):
         """
         d_train = xgb.DMatrix(self.x_train, label=self.y_train)
         cv_result = xgb.cv(
-            self.params, d_train, num_boost_round=1000, early_stopping_rounds=50, nfold=self.nfold
+            self.params, d_train, num_boost_round=self.num_boost_round,
+            early_stopping_rounds=self.early_stopping_rounds, nfold=self.nfold
         )
         return cv_result['test-{}-mean'.format(self.eval_metric)][cv_result.index[-1]]
 
 
 if __name__ == '__main__':
-    train = pd.read_csv('train.csv')
-    y = train['y']
-    x = train.drop(['y'], axis=1)
-    hyper = {
+    data = pd.read_csv('../tests/wine-quality/winequality-white.csv', delimiter=';')
+    y = data['quality']
+    x = data.drop(['quality'], axis=1)
+    genes = {
         'eta': 0.3, 'min_child_weight': 1, 'max_depth': 6, 'gamma': 0.0, 'max_delta_step': 0,
         'subsample': 1.0, 'colsample_bytree': 1.0, 'colsample_bylevel': 1.0, 'lambda': 1.0,
         'alpha': 0.0, 'scale_pos_weight': 1.0
     }
-    model = XgboostRegressor(x, y, hyper)
-    model.cross_validate()
+    model = XgboostRegressor(x, y, genes)
+    print model.cross_validate()
