6299520947c0191975b19549a972915de8a2e7c9
Gustavo Montamat <gmontamat@gmail.com>
2018-01-16 18:16:33 -0300

    Warn user if xgboost is not available

--- gentun/__init__.py
+++ gentun/__init__.py
@@ -1,7 +1,12 @@
 # Make public APIs available at top-level import
 from algorithms import GeneticAlgorithm
 from populations import Population
-from individuals import XgboostIndividual
-from models import XgboostModel
 from master import DistributedPopulation
 from worker import GentunWorker
+
+# Individual-Model pairs
+try:
+    from individuals import XgboostIndividual
+    from models import XgboostModel
+except ImportError:
+    print("Warning: install xgboost to use XgboostIndividual and XgboostModel.")
