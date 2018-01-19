56abf72c703dae1034fa86595d52a6e3de149920
Gustavo Montamat <gmontamat@gmail.com>
2018-01-19 01:51:27 -0300

    Enhance test cases

--- tests/test_grid_wine-quality.py
+++ tests/test_grid_wine-quality.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Test the genetic algorithm on a single machine over the
-'wine-quality' data using a random population as starter.
+'wine-quality' data using a grid as an initial population.
 """
 
 import os
@@ -9,12 +9,17 @@ import pandas as pd
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from gentun import GeneticAlgorithm, Population, XgboostIndividual
+from gentun import GeneticAlgorithm, GridPopulation, XgboostIndividual
 
 if __name__ == '__main__':
     data = pd.read_csv('./data/winequality-white.csv', delimiter=';')
     y_train = data['quality']
     x_train = data.drop(['quality'], axis=1)
-    pop = Population(XgboostIndividual, x_train, y_train, size=100, additional_parameters={'nfold': 3})
+    grid = {
+        'eta': [0.001, 0.005, 0.01, 0.015, 0.2],
+        'max_depth': range(3, 11),
+        'colsample_bytree': [0.80, 0.85, 0.90, 0.95, 1.0],
+    }
+    pop = GridPopulation(XgboostIndividual, x_train, y_train, genes_grid=grid, additional_parameters={'nfold': 3})
     ga = GeneticAlgorithm(pop)
     ga.run(10)
