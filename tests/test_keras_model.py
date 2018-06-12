7ab0104291e384058d42df6b7694c25ae711f5b2
Gustavo Montamat <gustavo.montamat@globant.com>
2018-06-12 18:04:31 -0300

    Add custom genes for testing model

--- tests/test_keras_model.py
+++ tests/test_keras_model.py
@@ -18,5 +18,12 @@ if __name__ == '__main__':
     lb.fit(range(max(mnist.target.astype('int')) + 1))
     y_train = lb.transform(mnist.target.astype('int'))
     x_train = mnist.data.reshape(mnist.data.shape[0], 28, 28, 1)
-    model = GeneticCnnModel(x_train, y_train, {'S_1': '', 'S_2': ''}, (20, 50), ((5, 5), (5, 5)), (28, 28, 1), 10)
+    model = GeneticCnnModel(
+        x_train, y_train,
+        {'S_1': '0111010001', 'S_2': '0100001011'},
+        (20, 50),
+        ((5, 5), (5, 5)),
+        (28, 28, 1),
+        10
+    )
     model.cross_validate()
