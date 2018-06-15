2d17b90a0b101296f1bb0bab34bfe9868abeed73
gmontamat <gmontamat@gmail.com>
2018-06-15 17:12:09 -0300

    Implement cross-validation and use a parameter for dense layer size

--- gentun/models/keras_models.py
+++ gentun/models/keras_models.py
@@ -7,74 +7,76 @@ import keras.backend as K
 
 from keras.layers import Input, Conv2D, Activation, Add, MaxPooling2D, Flatten, Dense, Dropout
 from keras.models import Model
+from sklearn.model_selection import KFold
 
 from .generic_models import GentunModel
 
 K.set_image_data_format('channels_last')
 
 
-def build_dag(x, connections, kernels):
-    # Get number of nodes (K_s) using the fact that K_s*(K_s-1)/2 == #bits
-    nodes = int((1 + (1 + 8 * len(connections)) ** 0.5) / 2)
-    # Separate bits by whose input they represent (GeneticCNN paper uses a dash)
-    ctr = 0
-    idx = 0
-    separated_connections = []
-    while idx + ctr < len(connections):
-        ctr += 1
-        separated_connections.append(connections[idx:idx + ctr])
-        idx += ctr
-    # Get outputs by node (dummy output ignored)
-    outputs = []
-    for node in range(nodes - 1):
-        node_outputs = []
-        for i, node_connections in enumerate(separated_connections[node:]):
-            if node_connections[node] == '1':
-                node_outputs.append(node + i + 1)
-        outputs.append(node_outputs)
-    outputs.append([])
-    # Get inputs by node (dummy input, x, ignored)
-    inputs = [[]]
-    for node in range(1, nodes):
-        node_inputs = []
-        for i, connection in enumerate(separated_connections[node - 1]):
-            if connection == '1':
-                node_inputs.append(i)
-        inputs.append(node_inputs)
-    # Build DAG
-    output_vars = []
-    all_vars = [None] * nodes
-    for i, (ins, outs) in enumerate(zip(inputs, outputs)):
-        if ins or outs:
-            if not ins:
-                tmp = x
-            else:
-                add_vars = [all_vars[i] for i in ins]
-                if len(add_vars) > 1:
-                    tmp = Add()(add_vars)
-                else:
-                    tmp = add_vars[0]
-            tmp = Conv2D(kernels, kernel_size=(3, 3), strides=(1, 1), padding='same')(tmp)
-            tmp = Activation('relu')(tmp)
-            all_vars[i] = tmp
-            if not outs:
-                output_vars.append(tmp)
-    if len(output_vars) > 1:
-        return Add()(output_vars)
-    return output_vars[0]
-
-
 class GeneticCnnModel(GentunModel):
 
-    def __init__(self, x_train, y_train, genes, kernels_per_layer, kernel_sizes, input_shape, classes):
+    def __init__(self, x_train, y_train, genes, kernels_per_layer, kernel_sizes,
+                 input_shape, dense_units, classes, plot_model=False):
         super(GeneticCnnModel, self).__init__(x_train, y_train)
-        self.model = self.build_model(genes, kernels_per_layer, kernel_sizes, input_shape, classes)
-        # Draw model to validate gene-to-DAG
-        # from keras.utils import plot_model
-        # plot_model(self.model, to_file='model.png')
+        self.model = self.build_model(genes, kernels_per_layer, kernel_sizes, input_shape, dense_units, classes)
+        if plot_model:
+            # Draw model to validate gene-to-DAG
+            from keras.utils import plot_model
+            plot_model(self.model, to_file='model.png')
 
     @staticmethod
-    def build_model(genes, kernels_per_layer, kernel_sizes, input_shape, classes):
+    def build_dag(x, connections, kernels):
+        # Get number of nodes (K_s) using the fact that K_s*(K_s-1)/2 == #bits
+        nodes = int((1 + (1 + 8 * len(connections)) ** 0.5) / 2)
+        # Separate bits by whose input they represent (GeneticCNN paper uses a dash)
+        ctr = 0
+        idx = 0
+        separated_connections = []
+        while idx + ctr < len(connections):
+            ctr += 1
+            separated_connections.append(connections[idx:idx + ctr])
+            idx += ctr
+        # Get outputs by node (dummy output ignored)
+        outputs = []
+        for node in range(nodes - 1):
+            node_outputs = []
+            for i, node_connections in enumerate(separated_connections[node:]):
+                if node_connections[node] == '1':
+                    node_outputs.append(node + i + 1)
+            outputs.append(node_outputs)
+        outputs.append([])
+        # Get inputs by node (dummy input, x, ignored)
+        inputs = [[]]
+        for node in range(1, nodes):
+            node_inputs = []
+            for i, connection in enumerate(separated_connections[node - 1]):
+                if connection == '1':
+                    node_inputs.append(i)
+            inputs.append(node_inputs)
+        # Build DAG
+        output_vars = []
+        all_vars = [None] * nodes
+        for i, (ins, outs) in enumerate(zip(inputs, outputs)):
+            if ins or outs:
+                if not ins:
+                    tmp = x
+                else:
+                    add_vars = [all_vars[i] for i in ins]
+                    if len(add_vars) > 1:
+                        tmp = Add()(add_vars)
+                    else:
+                        tmp = add_vars[0]
+                tmp = Conv2D(kernels, kernel_size=(3, 3), strides=(1, 1), padding='same')(tmp)
+                tmp = Activation('relu')(tmp)
+                all_vars[i] = tmp
+                if not outs:
+                    output_vars.append(tmp)
+        if len(output_vars) > 1:
+            return Add()(output_vars)
+        return output_vars[0]
+
+    def build_model(self, genes, kernels_per_layer, kernel_sizes, input_shape, dense_units, classes):
         x_input = Input(input_shape)
         x = x_input
         for layer, kernels in enumerate(kernels_per_layer):
@@ -85,22 +87,28 @@ class GeneticCnnModel(GentunModel):
             connections = genes['S_{}'.format(layer + 1)]
             # If at least one bit is 1, then we need to construct the Directed Acyclic Graph
             if not all([not bool(int(connection)) for connection in connections]):
-                x = build_dag(x, connections, kernels)
+                x = self.build_dag(x, connections, kernels)
                 # Output node
                 x = Conv2D(kernels, kernel_size=(3, 3), strides=(1, 1), padding='same')(x)
                 x = Activation('relu')(x)
             x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
         x = Flatten()(x)
-        x = Dense(500, activation='relu')(x)
+        x = Dense(dense_units, activation='relu')(x)
         x = Dropout(0.5)(x)
         x = Dense(classes, activation='softmax')(x)
         return Model(inputs=x_input, outputs=x, name='GeneticCNN')
 
     def cross_validate(self):
-        """Train model using n-fold cross validation and
-        return mean value of validation metric.
+        """Train model using k-fold cross validation and
+        return mean value of the loss.
         """
-        self.model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
         # TODO: cross-validation (here we are just doing train/validation split)
-        fit = self.model.fit(self.x_train, self.y_train, epochs=1, batch_size=32, verbose=1, validation_split=0.2)
-        return fit.history['loss'][-1]
+        # K-fold cross validation
+        splits = 5
+        loss = .0
+        kfold = KFold(n_splits=splits, shuffle=True)  # TODO: implement stratified k-fold
+        for train, test in kfold.split(self.x_train):
+            self.model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+            self.model.fit(self.x_train[train], self.y_train[train], epochs=2, batch_size=128, verbose=0)
+            loss += self.model.evaluate(self.x_train[test], self.y_train[test], verbose=0)[0] / splits
+        return loss
