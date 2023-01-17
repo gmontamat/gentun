6bb2d5a9a00cc6057080ed7c91a8c1d419a8d80e
Jędrzej <jedrzej.polaczek@gmail.com>
2023-01-17 16:53:23 +0100

    feat: initial draft of CNN with X0 step individual.

--- gentun/individuals.py
+++ gentun/individuals.py
@@ -282,3 +282,124 @@ class GeneticCnnIndividual(Individual):
             if new_connections != connections:
                 self.set_fitness(None)  # A mutation means the individual has to be re-evaluated
                 self.get_genes()[name] = new_connections
+
+
+class GeneticCnnX0Individual(Individual):  # TODO: rewrite it according to https://arxiv.org/pdf/1810.03522.pdf
+    """
+    Encoding of Neural Network leyars proposed in article:
+    NSGA-Net: Neural Architecture Search using Multi-Objective Genetic Algorithm
+    by Zhichao Lu, Ian Whalen, Vishnu Boddeti, Yashesh Dhebar, Kalyanmoy Deb, Erik Goodman and Wolfgang Banzhaf: 
+    https://arxiv.org/pdf/1703.01513.pdf 
+
+    It is variation of encoding of Neural Network leyars proposed in article: 
+    Genetic CNN 
+    by Lingxi Xie, Alan Yuille: 
+    https://arxiv.org/pdf/1703.01513.pdf 
+    """
+    def __init__(
+        self, 
+        x_train,  # TODO: add typing
+        y_train,  # TODO: add typing
+        genome: dict = None,  # genome is other name for individual
+        genes: dict = None,  # TODO: rename to chromosome?
+        crossover_rate: float = 0.3, 
+        mutation_rate: float = 0.1, 
+        nodes: tuple = (3, 5),
+        input_shape: tuple = (28, 28, 1), 
+        kernels_per_layer: tuple = (20, 50), 
+        kernel_sizes: tuple = ((5, 5), (5, 5)), 
+        dense_units: int = 500,
+        dropout_probability: float = 0.5, 
+        classes: int = 10, 
+        kfold: int = 5, 
+        epochs: tuple =(3,), 
+        learning_rate: float = (1e-3,), 
+        batch_size: int = 32
+    ):
+        # Set genomes and genes if none (we need to have something to work with)
+        if genome is None:
+            genome = self.generate_random_genome(nodes)
+        if genes is None:
+            genes = self.generate_random_genes(genome)
+
+        # Set individual's attributes
+        super(GeneticCnnIndividual, self).__init__(x_train, y_train, genome, genes, crossover_rate, mutation_rate)
+
+        # Set additional parameters which are not tuned
+        assert len(nodes) == len(kernels_per_layer) and len(kernels_per_layer) == len(kernel_sizes)
+
+        # Set genetic algorithms parameters
+        self.nodes = nodes
+        self.input_shape = input_shape
+        self.kernels_per_layer = kernels_per_layer
+        self.kernel_sizes = kernel_sizes
+        self.dense_units = dense_units
+        self.dropout_probability = dropout_probability
+        self.classes = classes
+        self.kfold = kfold
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+
+    @staticmethod
+    def generate_random_genome(nodes) -> dict:
+        """
+        Create and return random genome.
+        
+        Friendly reminder:
+        Genome is other name for individual.
+
+        Idea of how it works:
+        To create genome ...
+        """
+        return {'S_{}'.format(i + 1): int(K_s * (K_s - 1) / 2) for i, K_s in enumerate(nodes)}
+    
+    @staticmethod
+    def generate_random_genes(genome) -> dict:
+        """
+        Create and return random genes.
+        
+        Friendly reminder:
+        Chromosome is a set og genes. So refereing to genes we refering to chromosome.
+
+        Idea of how it works:
+        To create gene ...
+        """
+        genes = {}
+        for name, connections in genome.items():
+            genes[name] = ''.join([random.choice(['0', '1']) for _ in range(connections)])
+        return genes
+
+    def evaluate_fitness(self):
+        """Create model and perform cross-validation."""
+        model = GeneticCnnModel(
+            self.x_train, self.y_train, self.genes, self.nodes, self.input_shape, self.kernels_per_layer,
+            self.kernel_sizes, self.dense_units, self.dropout_probability, self.classes,
+            self.kfold, self.epochs, self.learning_rate, self.batch_size
+        )
+        self.fitness = model.cross_validate()
+
+    def get_additional_parameters(self):
+        return {
+            'nodes': self.nodes,
+            'input_shape': self.input_shape,
+            'kernels_per_layer': self.kernels_per_layer,
+            'kernel_sizes': self.kernel_sizes,
+            'dense_units': self.dense_units,
+            'dropout_probability': self.dropout_probability,
+            'classes': self.classes,
+            'kfold': self.kfold,
+            'epochs': self.epochs,
+            'learning_rate': self.learning_rate,
+            'batch_size': self.batch_size
+        }
+
+    def mutate(self):
+        """Mutate instance's genes with a certain probability."""
+        for name, connections in self.get_genes().items():
+            new_connections = ''.join([
+                str(int(int(byte) != (random.random() < self.mutation_rate))) for byte in connections
+            ])
+            if new_connections != connections:
+                self.set_fitness(None)  # A mutation means the individual has to be re-evaluated
+                self.get_genes()[name] = new_connections
