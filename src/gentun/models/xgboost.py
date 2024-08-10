9adde9883481a59aad52fa5dd661b4a54b00ad3c
Gustavo Montamat <gmontamat@gmail.com>
2024-08-10 19:42:29 -0400

    Complete xgboost cross-validation model

--- src/gentun/models/xgboost.py
+++ src/gentun/models/xgboost.py
@@ -13,39 +13,39 @@ class XGBoostCV(Model):
 
     def __init__(
             self,
+            metrics: Union[str, Tuple[str, ...]],
             num_boost_round: int = 10,
             nfold: int = 3,
             stratified: bool = False,
-            metrics: Union[str, Tuple[str, ...]] = "",
             early_stopping_rounds: Optional[int] = None,
             **kwargs):
         """
         Booster params reference:
-        - https://xgboost.readthedocs.io/en/stable/parameter.html#general-parameters
-        - https://xgboost.readthedocs.io/en/stable/parameter.html#parameters-for-tree-booster
-        - https://xgboost.readthedocs.io/en/stable/parameter.html#learning-task-parameters
+            - https://xgboost.readthedocs.io/en/stable/parameter.html#general-parameters
+            - https://xgboost.readthedocs.io/en/stable/parameter.html#parameters-for-tree-booster
+            - https://xgboost.readthedocs.io/en/stable/parameter.html#learning-task-parameters
         """
         super().__init__()
         # Cross-validation params
+        self.metrics = metrics  # XGBoost will evaluate this metric
         self.num_boost_round = num_boost_round
         self.nfold = nfold
         self.stratified = stratified
-        self.metrics = metrics
         self.early_stopping_rounds = early_stopping_rounds
-        self.kwargs = kwargs  # Booster params
-        if "verbosity" not in self.kwargs:
-            self.kwargs["verbosity"] = 0
+        self.booster_params = kwargs  # Booster params
+        if "verbosity" not in self.booster_params:
+            self.booster_params["verbosity"] = 0  # By default, be silent
 
     def evaluate(self, x_train: np.ndarray, y_train: np.ndarray):
         """
         Use xgboost cross-validation with given parameters.
-        API reference:
+        xgboost.cv API reference:
             - https://xgboost.readthedocs.io/en/stable/python/python_api.html#xgboost.cv
         """
         import xgboost as xgb  # We import here so that xgboost is optional
         d_train = xgb.DMatrix(x_train, label=y_train)
         cv_result = xgb.cv(
-            self.kwargs,
+            self.booster_params,
             d_train,
             num_boost_round=self.num_boost_round,
             nfold=self.nfold,  # the "k" in k-fold cross-validation
@@ -53,4 +53,8 @@ class XGBoostCV(Model):
             metrics=self.metrics,
             early_stopping_rounds=self.early_stopping_rounds,
         )
-        return cv_result[f'test-{self.metrics[-1]}-mean'][-1]
+        if isinstance(self.metrics, list):
+            metric = self.metrics[-1]
+        else:
+            metric = self.metrics
+        return cv_result[f"test-{metric}-mean"][-1]
