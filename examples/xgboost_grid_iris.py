dd5a1cdd3e9f4a698dc2e1da3b2236c5890f2b08
Gustavo Montamat <gmontamat@gmail.com>
2024-08-19 14:18:28 -0400

    Add grid search example

---
+++ examples/xgboost_grid_iris.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+"""
+Test a grid search on a single node
+with the iris dataset using xgboost.
+"""
+
+import csv
+import os
+import sys
+from typing import Tuple
+
+import numpy as np
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+
+
+def parse_iris(file_name: str) -> Tuple[np.ndarray, np.ndarray]:
+    """Load iris.data into X, y arrays."""
+    x = []
+    y = []
+    with open(file_name, "r", encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if not row:
+                continue
+            features = [float(val) for val in row[:-1]]
+            targets = 0 if "setosa" in row[-1] else 1 if "versicolor" in row[-1] else 2
+            x.append(features)
+            y.append(targets)
+    x = np.array(x).astype(np.float64)
+    y = np.array(y)
+    return x, y
+
+
+if __name__ == "__main__":
+    from gentun.algorithms import Tournament
+    from gentun.genes import RandomChoice, RandomLogUniform
+    from gentun.models.xgboost import XGBoostCV
+    from gentun.populations import Grid
+
+    # xgboost hyperparameters
+    genes = [
+        RandomLogUniform("learning_rate", minimum=0.001, maximum=0.1, base=10),
+        RandomChoice("max_depth", range(3, 11)),
+        RandomChoice("min_child_weight", range(11)),
+    ]
+    gene_samples = [10, 8, 11]
+    # xgboost static parameters
+    kwargs = {
+        "booster": "gbtree",
+        "device": "cpu",
+        "objective": "multi:softmax",
+        "metrics": "mlogloss",  # The metric we want to minimize with the algorithm
+        "num_class": 3,
+        "nfold": 5,
+        "num_boost_round": 5000,
+        "early_stopping_rounds": 100,
+    }
+
+    # Fetch training data
+    x_train, y_train = parse_iris("iris.data")
+    # Run genetic algorithm on a grid population for 1 generation
+    population = Grid(genes, XGBoostCV, x_train, y_train, gene_samples, **kwargs)
+    algorithm = Tournament(population)
+    algorithm.run(1, maximize=False)
