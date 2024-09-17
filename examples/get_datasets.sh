5fd3a3461642590e407b774d82a7abfdf6c2442d
Gustavo Montamat <gmontamat@gmail.com>
2024-09-17 15:02:16 -0400

    Add CIFAR10 download

--- examples/get_datasets.sh
+++ examples/get_datasets.sh
@@ -10,4 +10,10 @@ if [ ! -e mnist.npz ]; then
     wget https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz
 fi
 
+if [ ! -e cifar-10-python.tar.gz ]; then
+    echo "Downloading CIFAR-10 dataset..."
+    wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+    tar -xvzf cifar-10-python.tar.gz
+fi
+
 echo "Done!"
